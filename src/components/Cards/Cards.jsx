247920b27223bc362bd55807660d52617fb9dec0
Swastikyadav <swastikyadav.dev@gmail.com>
2020-04-24 01:44:21 +0530

    build basic fetchData function

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = ({ data }) => {
+  return (
+    <>
+    <h1>Cards</h1>
+    {console.log(data)}
+    </>
+  )
+}
+
+export default Card;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Chart = () => {
+  return (
+    <h1>Charts</h1>
+  )
+}
+
+export default Chart;
