47801145a3a46cd2b6786e30d4acfd5a3c45c09b
Swastikyadav <swastikyadav.dev@gmail.com>
2020-04-24 03:42:04 +0530

    fetched global daily data

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,27 @@
 import axios from "axios";
 
-const url = "https://covid19.mathdro.id/api";
+const baseUrl = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(baseUrl);
 
     return { confirmed, recovered, deaths, lastUpdate }
   } catch(error) {
+
+  }
+}
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/daily`);
+    
+    return data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }))
+   } catch(error) {
 
   }
 }
