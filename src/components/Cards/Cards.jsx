1b2601e3f09d5ce3d67d939339fe633e27a561b6
Swastikyadav <swastikyadav.dev@gmail.com>
2020-04-24 15:48:36 +0530

    refactoring

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import CountUp from "react-countup";
 import cx from "classnames";
+import { capitalize } from "../../utils";
 
 import styles from "./Cards.module.css";
 
@@ -9,56 +10,36 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   if (!confirmed) {
     return "Loading..."
   }
+
+  const cardArr = [
+    {cardName: "confirmed", value: confirmed.value, color: "rgba(0, 0, 255, 0.5)", body2: "Number of infected people from COVID-19"},
+    {cardName: "recovered", value: recovered.value, color: "rgba(0, 255, 0, 0.5)", body2: "Number of recovered people from COVID-19"},
+    {cardName: "deaths", value: deaths.value, color: "rgba(255, 0, 0, 0.5)", body2: "Number of deaths caused by COVID-19"},
+  ]
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>Confirmed</Typography>
-            <Typography style={{ color: "rgba(0, 0, 255, 0.5)" }} variant="h5">
-              <CountUp
-                start={0}
-                end={confirmed.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of infected people from COVID-19</Typography>
-          </CardContent>
-        </Grid>
-
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-            <Typography style={{ color: "rgba(0, 255, 0, 0.5)" }} variant="h5">
-              <CountUp
-                start={0}
-                end={recovered.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of recovered people from COVID-19</Typography>
-          </CardContent>
-        </Grid>
-
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-            <Typography style={{ color: "rgba(255, 0, 0, 0.5)" }} variant="h5">
-              <CountUp
-                start={0}
-                end={deaths.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of deatch caused by COVID-19</Typography>
-          </CardContent>
-        </Grid>
+        {cardArr.map((eachCard, idx) => {
+          const { cardName, color, body2, value } = eachCard;
+          return (
+            <Grid item component={Card} xs={12} md={3} key={idx} className={cx(styles.card, styles[cardName])}>
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>{capitalize(cardName)}</Typography>
+                <Typography style={{ color: color.toString() }} variant="h5">
+                  <CountUp
+                    start={0}
+                    end={value}
+                    duration={2.5}
+                    separator=","
+                  />
+                </Typography>
+                <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
+                <Typography variant="body2">{body2}</Typography>
+              </CardContent>
+            </Grid>
+          );
+        })}
       </Grid>
     </div>
   )
